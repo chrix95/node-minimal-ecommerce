0fa7728e4e59da5721398b78d3f9c8f2c7539a82
Christopher Okokon Ntuk <engchris95@gmail.com>
2021-12-13 12:38:40 +0100

    Fix to the stripe payment

--- src/routes/stripe.js
+++ src/routes/stripe.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 router.post('/payment', (req, res) => {
-    stripe.charge.create({
+    console.log(stripe);
+    stripe.charges.create({
         source: req.body.tokenId,
         amount: req.body.amount,
         currency: 'usd'
