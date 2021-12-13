32b1a41223b6f586d58aa20a2cbd80f86b1c87b0
Christopher Okokon Ntuk <engchris95@gmail.com>
2021-12-13 13:25:57 +0100

    Fix on the stripe payment

--- src/routes/stripe.js
+++ src/routes/stripe.js
@@ -1,15 +1,22 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const router = require('express').Router();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 router.post('/payment', (req, res) => {
-    console.log(stripe);
+    console.log(req.body);
     stripe.charges.create({
         source: req.body.tokenId,
         amount: req.body.amount,
         currency: 'usd'
     }, (stripeErr, stripeRes) => {
-        stripeErr && res.status(500).send({ 'success': false, 'message': stripeErr });
-        res.status(200).json({ 'success': true, 'message': stripeRes });
+        if (stripeErr) {
+            console.log(stripeErr);
+            res.status(500).json({ 'success': false, 'message': stripeErr });
+        } else {
+            console.log(stripeRes);
+            res.status(200).json({ 'success': true, 'data': stripeRes });
+        }
     })
 })
 
