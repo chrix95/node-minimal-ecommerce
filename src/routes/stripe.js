6325742c22f3b4a0dc938ed6908a6cd1c675e36f
Christopher Okokon Ntuk <engchris95@gmail.com>
2021-12-13 13:26:16 +0100

    Fix on the stripe payment

--- src/routes/stripe.js
+++ src/routes/stripe.js
@@ -4,17 +4,14 @@ const router = require('express').Router();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 router.post('/payment', (req, res) => {
-    console.log(req.body);
     stripe.charges.create({
         source: req.body.tokenId,
         amount: req.body.amount,
         currency: 'usd'
     }, (stripeErr, stripeRes) => {
         if (stripeErr) {
-            console.log(stripeErr);
             res.status(500).json({ 'success': false, 'message': stripeErr });
         } else {
-            console.log(stripeRes);
             res.status(200).json({ 'success': true, 'data': stripeRes });
         }
     })
